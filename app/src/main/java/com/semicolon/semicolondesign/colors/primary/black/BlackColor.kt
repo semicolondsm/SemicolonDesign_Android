242d13d9bb6b1a4ca813b22ea1436c126edb2f55
Jaewon <turitus@naver.com>
2021-09-03 16:52:51 +0900

    ⚡️:: black method

---
+++ app/src/main/java/com/semicolon/semicolondesign/colors/primary/black/BlackColor.kt
@@ -0,0 +1,9 @@
+package com.semicolon.semicolondesign.colors.primary.black
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.graphics.Color
+
+@Composable
+@ReadOnlyComposable
+fun black(): Color = black
