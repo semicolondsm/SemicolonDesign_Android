72d8a45a791fd5e499bebc7c407178914417fdee
Jaewon <turitus@naver.com>
2021-09-03 16:53:16 +0900

    ⚡️:: indigo method

---
+++ app/src/main/java/com/semicolon/semicolondesign/colors/secondary/indigo/IndigoColor.kt
@@ -0,0 +1,30 @@
+package com.semicolon.semicolondesign.colors.secondary.indigo
+
+import androidx.compose.ui.graphics.Color
+
+
+fun indigo(number: Int): Color {
+    return when (number) {
+        50 -> indigo50
+    }
+}
+
+fun indigo50(): Color = indigo50
+
+fun indigo100(): Color = indigo100
+
+fun indigo200(): Color = indigo200
+
+fun indigo300(): Color = indigo300
+
+fun indigo400(): Color = indigo400
+
+fun indigo500(): Color = indigo500
+
+fun indigo600(): Color = indigo600
+
+fun indigo700(): Color = indigo700
+
+fun indigo800(): Color = indigo800
+
+fun indigo900(): Color = indigo900
