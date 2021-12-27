7e9c3a2f473929b24b4961e60b21f1a23f0f9b52
shw0471 <shw0471@naver.com>
2021-12-27 14:42:58 +0900

    📑 :: add TestActivity

---
+++ app/src/main/java/com/semicolon/semicolondesign/TestActivity.kt
@@ -0,0 +1,11 @@
+package com.semicolon.semicolondesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
