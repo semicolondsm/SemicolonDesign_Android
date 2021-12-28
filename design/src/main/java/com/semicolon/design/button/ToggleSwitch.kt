e88c4f8522132b93e295567c4648de39556912b3
shw0471 <shw0471@naver.com>
2021-12-28 14:09:43 +0900

    ⚡️ :: update toggle switch

---
+++ design/src/main/java/com/semicolon/design/button/ToggleSwitch.kt
@@ -0,0 +1,71 @@
+package com.semicolon.design.button
+
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.semicolon.design.color.primary.gray.gray100
+import com.semicolon.design.color.primary.purple.purple400
+import com.semicolon.design.color.primary.white.white
+
+@Composable
+fun ToggleSwitch(
+    modifier: Modifier = Modifier,
+    defaultState: ToggleState = ToggleState.TOGGLE_OFF,
+    onToggleOn: () -> Unit = {},
+    onToggleOff: () -> Unit = {}
+) {
+    val isToggleOn = remember { mutableStateOf(defaultState == ToggleState.TOGGLE_ON) }
+    val backgroundColor: Color by animateColorAsState(if (isToggleOn.value) purple400 else gray100)
+    val spaceSize: Dp by animateDpAsState(if (isToggleOn.value) 20.dp else 0.dp)
+    Row(
+        modifier = modifier
+            .height(20.dp)
+            .width(40.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(backgroundColor)
+            .padding(1.dp)
+            .clickable(
+                interactionSource = MutableInteractionSource(),
+                indication = null
+            ) {
+                isToggleOn.value = !isToggleOn.value
+                if (isToggleOn.value) onToggleOn() else onToggleOff()
+            }
+    ) {
+        Spacer(modifier = Modifier.size(spaceSize))
+        Spacer(
+            modifier = Modifier
+                .size(18.dp)
+                .clip(RoundedCornerShape(9.dp))
+                .background(white)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ToggleSwitchPreview() {
+    ToggleSwitch(
+        defaultState = ToggleState.TOGGLE_OFF
+    )
+}
+
+enum class ToggleState {
+    TOGGLE_ON, TOGGLE_OFF
+}
